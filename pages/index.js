61d68367eb9cc57d3a5e5c1c656dd655193e5bdb
LuisGtk <luisgonzalez189@gmail.com>
2021-08-12 10:34:34 -0400

    made comp files

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>NYC-Reports</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <h1>Greetings</h1>
+
+
+
+
+
+    </div>
+  )
+}
