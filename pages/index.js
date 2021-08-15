fbae397c26f94380538776f2701fb0c30f6bd4ae
LuisGtk <luisgonzalez189@gmail.com>
2021-08-15 14:15:55 -0400

    changed style of buttons, adding finishing touches, about to start deployment

--- pages/index.js
+++ pages/index.js
@@ -23,19 +23,19 @@ export default function Home(props) {
         </Head>
         {/* making the title a link to go back to home page */}
         <Link href="/"><a className="font-extrabold m-l:4 text-2xl"> NEW YORK CITY 311-REPORTS </a></Link>
-        <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
+        <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center '>
           {/* map through my requests.js to grab my key (variable) and the value (borough and url) */}
           {Object.entries(requests).map(([key, { borough, url }]) => (
             <h2 key={key} onClick={() => onGetData(borough, url)}
               className=" cursor-pointer bg-blue-600 hover:bg-blue-500 text-white
-                py-4 border-b-4 border-blue-800 hover:border-blue-500 rounded m-6 tracking-widest">{borough}</h2>
+                py-4 border-b-4 border-blue-800 hover:border-blue-500 rounded-full m-6 tracking-widest text-center">{borough}</h2>
           ))}
         </div>
         {/* tabel-head element */}
         <table className="table-auto border-separate ">
           <thead>
             <tr>
-              {/* headers to the tabel */}
+              {/* "headers" to the tabel */}
               <th className="px-4 py-3">Agent</th>
               <th >Agent Name</th>
               <th >Borough</th>
@@ -48,7 +48,7 @@ export default function Home(props) {
             </tr>
           </thead>
           <tbody >
-            {/* all the called data */}
+            {/* all the called data  LSO available to see in console*/}
             {
               result.map((row, index) => (
                 <tr className="bg-blue-600 border-separate border font-bold tracking-widest " key={index}>
